ae586b16a00bae72fe44274561ee724419626a22
Mohammad Jumah <mohammad_jumah@outlook.com>
2023-12-04 22:35:04 +0000

    adding book

--- books.sql
+++ books.sql
@@ -5,3 +5,5 @@ CREATE TABLE books (`id` BIGINT NOT NULL AUTO_INCREMENT, `title` VARCHAR(2048) N
 INSERT INTO books (title, author, image_pathLocation) Values ('Exploring War and Weapons', 'Brian Williams', 'images/resized-ExploringWarandWeapons.jpg');
 INSERT INTO books (title, author, image_pathLocation) Values ('Exploring Animal Journeys', 'Theodore Rowland-Entwistle', 'images/resized-ExploringAnimalJourneys.jpg');
 INSERT INTO books (title, author, image_pathLocation) Values ('Exploring The World Of Robots', 'Jonathn Rutland', 'resized-ExploringTheWorldOfRobots.jpg');
+INSERT INTO books (title, author, image_pathLocation) Values ('Exploring Under The Sea', 'Brian Williams', 'images/resized-ExploringUnderTheSea.jpg');
+INSERT INTO books (title, author, image_pathLocation) Values ('Exploring The Age Of Steam', 'Jonathan Rutland', 'images/resized-ExploringTheAgeOfSteam.jpg');
